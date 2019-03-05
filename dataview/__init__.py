9a06018c9fadf07643b978f6ce96eee24d5eaf21
nikhartman <nik.hartman@gmail.com>
2019-03-04 16:19:31 -0800

    more restructuring

--- dataview/__init__.py
+++ dataview/__init__.py
@@ -1,1 +1,10 @@
+import dash
 
+app = dash.Dash(__name__)
+app.title='Data Viewer'
+
+app.config.suppress_callback_exceptions = True
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+
+import dataview.main
