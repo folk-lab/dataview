97f9f263df76323f4d0cd9ea466a5f19b3f6b408
nikhartman <nik.hartman@gmail.com>
2019-03-05 15:14:03 -0800

    back to server!

--- dataview/__init__.py
+++ dataview/__init__.py
@@ -1,19 +1,41 @@
 import dash
 
-app = dash.Dash(__name__)
-app.title='Data Viewer'
+app_prefix = ''
 
+#app = dash.Dash(__name__)
+app = dash.Dash(
+    __name__,
+    url_base_pathname=app_prefix + '/',
+    assets_url_path=app_prefix + '/assets',
+    meta_tags=[
+        {
+            'charset': 'utf-8',
+        },
+        {
+            'http-equiv': 'X-UA-Compatible',
+            'content': 'IE=edge'
+        },
+        # needed for iframe resizer
+        {
+            'name': 'viewport',
+            'content': 'width=device-width, initial-scale=1'
+        },
+    ])
 app.config.suppress_callback_exceptions = True
-app.config.update({
-    # remove the default of '/'
-    'routes_pathname_prefix': '',
-
-    # remove the default of '/'
-    'requests_pathname_prefix': ''
-})
-
-app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
+app.css.config.serve_locally = True
+
+title = 'Data Viewer'
+
+app.title = title
+
+# app.config.update({
+#     # remove the default of '/'
+#     'routes_pathname_prefix': '',
+#
+#     # remove the default of '/'
+#     'requests_pathname_prefix': ''
+# })
 
 from dataview import main
 from dataview import filesystem
