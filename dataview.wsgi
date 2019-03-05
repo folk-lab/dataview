31c5c4f1130b8eda89fb3a197e9953ab5020ff9e
nikhartman <nik.hartman@gmail.com>
2019-03-05 14:38:11 -0800

    some important .wsgi updates

--- dataview.wsgi
+++ dataview.wsgi
@@ -5,4 +5,5 @@ activate_this = '/srv/www/dataview/venv/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
-from dataview import server as application
+from dataview import app
+application = app.server
--- dataview/__init__.py
+++ dataview/__init__.py
@@ -3,6 +3,7 @@ import dash
 app = dash.Dash(__name__)
 app.title='Data Viewer'
 
+app.config.requests_pathname_prefix = ''
 app.config.suppress_callback_exceptions = True
 app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
@@ -10,5 +11,3 @@ app.scripts.config.serve_locally = True
 from dataview import main
 from dataview import filesystem
 from dataview import dataset
-
-server = app.server
