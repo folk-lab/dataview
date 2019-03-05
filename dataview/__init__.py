913187d8d35f0530ee46f4fb50d64dd174ec1066
nikhartman <nik.hartman@gmail.com>
2019-03-05 15:44:49 -0800

    found assets but still not the data

--- dataview/__init__.py
+++ dataview/__init__.py
@@ -1,12 +1,7 @@
 import dash
 
-app_prefix = ''
-
-#app = dash.Dash(__name__)
 app = dash.Dash(
     __name__,
-    url_base_pathname=app_prefix + '/',
-    assets_url_path=app_prefix + '/assets',
     meta_tags=[
         {
             'charset': 'utf-8',
@@ -21,21 +16,21 @@ app = dash.Dash(
             'content': 'width=device-width, initial-scale=1'
         },
     ])
+
+app.config.update({
+     # remove the default of '/'
+     'routes_pathname_prefix': '',
+
+     # remove the default of '/'
+     'requests_pathname_prefix': ''
+     })
+
+app._assets_url_path = 'dataview/assets'
 app.config.suppress_callback_exceptions = True
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
 
-title = 'Data Viewer'
-
-app.title = title
-
-# app.config.update({
-#     # remove the default of '/'
-#     'routes_pathname_prefix': '',
-#
-#     # remove the default of '/'
-#     'requests_pathname_prefix': ''
-# })
+app.title = 'Data Viewer'
 
 from dataview import main
 from dataview import filesystem
