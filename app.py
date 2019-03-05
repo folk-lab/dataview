33da9e0d84bdac5ad5513768d008cb2e803e54be
nikhartman <nik.hartman@gmail.com>
2019-03-04 16:00:42 -0800

    restructure

--- app.py
+++
@@ -1,8 +0,0 @@
-import dash
-
-app = dash.Dash(__name__)
-app.title='Data Viewer'
-
-app.config.suppress_callback_exceptions = True
-app.css.config.serve_locally = True
-app.scripts.config.serve_locally = True
--- dataview.wsgi
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(0, '/srv/www/DashViewMeasurements')
-from index import server as application
