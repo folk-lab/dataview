7044795d511d748c62eba200b135fe4612e0251c
nikhartman <nik.hartman@gmail.com>
2019-03-05 17:56:46 -0800

    gotta quit for the day

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 config = {
 	'FileServerProtocol':	'local', #local, samba, ...
-	# 'LocalRootDirectory':	'/measurement-data',
+	'LocalRootDirectory':	'/measurement-data',
 	'ServerSubdirectory':   'dataview',
-	'LocalRootDirectory':   '/Users/nik/Dropbox/data_2018',
+	#'LocalRootDirectory':   '/Users/nik/Dropbox/data_2018',
 	# 'SambaServername':		'phys-jumbo.physik.unibas.ch',
 	# 'SambaServiceName':		'zum$',
 	# 'SambaServerIP':		'10.34.8.3',
