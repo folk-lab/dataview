6fe2c1e9fea2127d290e34d82752fba5daeaac41
Mohammad Samani <m@msamani.ca>
2019-04-29 13:57:12 +0200

    Simplified dropdowns. Assume x_array and y_array and number of dimensions can be extracted automatically accurately.

--- dataview/dataset.py
+++ dataview/dataset.py
@@ -42,7 +42,7 @@ def get_dataset_menus(file_path):
 	with h5py.File(file_path, 'r') as f: # load file object
 
 		# find all datasets in hdf5 tree
-		names = ['-'] # this array will hold the datasets
+		names = [] # this array will hold the datasets
 		def find_datasets(name, h5obj):
 			""" this function is called as visititems walks the filetree """
 			if type(f[name])==h5py._hl.dataset.Dataset:
@@ -50,35 +50,25 @@ def get_dataset_menus(file_path):
 
 		f['/'].visititems(find_datasets) # collect list of datasets
 		xdefault, ydefault = find_default_arrays(names)
-		zdefault = '-'
+		
+		displaynames = []
+		for i,n in enumerate(names):
+			if not n == xdefault and not n == ydefault:
+				displaynames.append(n)
 		
 		return html.Div(
-		       [html.Div(id='file-path', children=file_path, style={'display': 'none'}),
-		        html.Div([
-		        dcc.Dropdown(
-		            id='x-dropdown',
-		            options=[{'label':name, 'value':name} for name in names],
-		            value = xdefault,
-					clearable=False,),
-		            ],className='dropdown'
-				),
-		        html.Div([
-		        dcc.Dropdown(
-		            id='y-dropdown',
-					options=[{'label':name, 'value':name} for name in names],
-		            value = ydefault,
-					clearable=False,),
-		            ],className='dropdown'
-				),
+				[html.Div(id='file-path', children=file_path, style={'display': 'none'}),
+				html.Div(id='x-data', children=xdefault, style={'display': 'none'}),
+				html.Div(id='y-data', children=ydefault, style={'display': 'none'}),
 				html.Div([
-		        dcc.Dropdown(
-		            id='z-dropdown',
-					options=[{'label':name, 'value':name} for name in names],
-		            value = zdefault,
-					clearable=False,),
-		            ],className='dropdown'
-				),
-		    ],
+					dcc.Dropdown(
+						id='data-dropdown',
+						options=[{'label':name, 'value':name} for name in displaynames],
+						value = displaynames[0],
+						clearable=False,),
+						],
+					className='data-dropdown-div'
+				)]
 		)
 
 def check_data_shapes(x, y, z=None):
@@ -93,83 +83,92 @@ def check_data_shapes(x, y, z=None):
 
 @app.callback(dash.dependencies.Output('plot-area', 'children'),
 	[dash.dependencies.Input('file-path', 'children'),
-	dash.dependencies.Input('x-dropdown', 'value'),
-	dash.dependencies.Input('y-dropdown', 'value'),
-	dash.dependencies.Input('z-dropdown', 'value')])
-def update_plot(fname, xname, yname, zname):
-	_ds_logger.debug(f'updating plot: {fname} {xname} {yname} {zname}')
+	dash.dependencies.Input('x-data', 'children'),
+	dash.dependencies.Input('y-data', 'children'),
+	dash.dependencies.Input('data-dropdown', 'value')])
+def update_plot(fname, xname, yname, dname):
+	_ds_logger.debug(f'updating plot: {fname} {xname} {yname} {dname}')
 	with h5py.File(fname, 'r') as f: # load file object
 
 		# load x and y data
-		if xname!='-' and yname!='-':
+		if xname!='-':
 			x = f[xname][:]
+			_ds_logger.debug('x.ndim={0} shape={1}'.format(x.ndim, x.shape))
+		if yname!='-':
 			y = f[yname][:]
+			_ds_logger.debug('y.ndim={0} shape={1}'.format(y.ndim, y.shape))
 
-			# check if these arrays are 1d
-			if (x.ndim==1 or x.shape[0]==1) and (y.ndim==1 or y.shape[0]==1):
-				x = np.ravel(x)
-				y = np.ravel(y)
-			else:
-				return html.P('ShapeError: x and y should be 1d arrays')
-		else:
-			return html.P('MissingDataError: Need both x and y arrays')
+		d = f[dname][:]
+		_ds_logger.debug('Data dimensions: {0} shape: {1}'.format(d.ndim, d.shape))
+		if d.ndim == 1:
+			# Fucking one dimensional!
+			return _plot1d(x, d)
+		if d.ndim == 2:
+			# Respectfully two dimensional!
+			return _plotsd(x, y, d)
+		
+			# # check if these arrays are 1d
+			# if (x.ndim==1 or x.shape[0]==1) and (y.ndim==1 or y.shape[0]==1):
+				# x = np.ravel(x)
+				# y = np.ravel(y)
+			# else:
+				# return html.P('ShapeError: x and y should be 1d arrays')
+		# else:
+			# return html.P('MissingDataError: Need both x and y arrays')
 
-		# load z data
-		if zname=='-':
-			z = None
-			return _plot1d(x, y)
-		else:
-			z = f[zname][:]
-			if (z.ndim==1 or z.shape[0]==1):
-				return _plot1d(x, y, np.ravel(z))
-			elif z.shape[1]==0:
-				return _plot1d(y, np.ravel(z))
-			else:
-				return _plot2d(x,y,z)
+		# # load z data
+		# if zname=='-':
+			# z = None
+			# return _plot1d(x, y)
+		# else:
+			# z = f[zname][:]
+			# if (z.ndim==1 or z.shape[0]==1):
+				# return _plot1d(x, y, np.ravel(z))
+			# elif z.shape[1]==0:
+				# return _plot1d(y, np.ravel(z))
+			# else:
+				# return _plot2d(x,y,z)
 
-def _plot1d(x, y):
-	if check_data_shapes(x, y):
-		data = go.Scatter( x = x, y = y, mode = 'lines')
-		graph_elem = dcc.Graph(
-	        figure={
-	            'data': data,
-	            'layout': go.Layout(
-	                # xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-	                # yaxis={'title': 'Life Expectancy'},
-	                # margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-	                # legend={'x': 0, 'y': 1},
-	                hovermode='closest'
-	            )
-	        }
-	    )
-
-		return graph_elem
-	else:
+def _plot1d(x, d):
+	if not check_data_shapes(x, d):
 		return html.P('ShapeError: x and y array shapes not consistent')
+	
+	data = go.Scatter( x = x, y = d, mode = 'lines')
+	graph_elem = dcc.Graph(
+		figure={
+			'data': [data],
+			'layout': go.Layout(
+				# xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+				# yaxis={'title': 'Life Expectancy'},
+				# margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+				# legend={'x': 0, 'y': 1},
+				hovermode='closest'
+			)
+		}
+	)
 
-def _plot2d(x, y, z, **kwargs):
+	return graph_elem		
 
-	if check_data_shapes(x, y, z=z):
-		data = go.Heatmap(
-			x = x,
-			y = y,
-			z = z,
-			colorscale='Viridis',
-		)
-
-		graph_elem = dcc.Graph(
-	        figure={
-	            'data': [data],
-	            'layout': go.Layout(
-	                # xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-	                # yaxis={'title': 'Life Expectancy'},
-	                # margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-	                # legend={'x': 0, 'y': 1},
-	                hovermode='closest'
-	            )
-	        }
-	    )
-
-		return graph_elem
-	else:
+def _plot2d(x, y, d):
+	if not check_data_shapes(x, y, z=d):
 		return html.P('ShapeError: x, y, and z array shapes not consistent')
+		
+	data = go.Heatmap(
+		x = x,
+		y = y,
+		z = d,
+		colorscale='Viridis',
+	)
+	graph_elem = dcc.Graph(
+		figure={
+			'data': [data],
+			'layout': go.Layout(
+				# xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+				# yaxis={'title': 'Life Expectancy'},
+				# margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+				# legend={'x': 0, 'y': 1},
+				hovermode='closest'
+			)
+		}
+	)
+	return graph_elem
