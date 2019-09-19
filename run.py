154feba51228f02cee2de2e9639f6cd17b78cc0a
Mohammad Samani <m@msamani.ca>
2019-09-19 12:52:33 +0200

    If run locally, local port number comes from config.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from dataview import app
+from config import config
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8082, threaded=True)
+    app.run_server(host=config['ServerHostName'], port=config['ServerPort'], threaded=True)
